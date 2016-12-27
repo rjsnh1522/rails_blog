d0be125bc0b5671c7c241a7757098c6894168b2b
logan <rjsnh1522@gmail.com>
2016-12-27 17:44:03 +0530

    Added Editd Delete Functionality in project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -28,6 +28,23 @@ class ProjectsController < ApplicationController
   end
 
 
+  def edit
+
+  end
+
+  def update
+    if @project.update project_params
+      redirect_to @project,notice: "Nice Logan! Project Updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+   @project.destroy
+    redirect_to projects_path
+  end
+
 
 
   private
